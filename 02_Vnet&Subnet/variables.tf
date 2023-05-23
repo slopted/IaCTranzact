ee88fd98c36dd804c2128c5de87a301eee57cf23
Slopted <slopted@gmail.com>
2023-05-23 11:22:32 -0400

    add terraform snet corrections

--- 02_Vnet&Subnet/variables.tf
+++ 02_Vnet&Subnet/variables.tf
@@ -8,17 +8,20 @@ variable "name_prefix" {
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable "rg_name" {
-  default     = "${var.name_prefix}_rg"
-  description = "Name of the resource group."
-}
+#variable "rg_name" {
+#  type     = string
+  #default     = "${var.name_prefix}_rg"
+#  description = "Name of the resource group."
+#}
 
-variable "nsg_name" {
-  default     = "${var.name_prefix}_nsg"
-  description = "Name of the Network Security Group."
-}
+#variable "nsg_name" {
+#  type     = string
+#  #default     = "${var.name_prefix}_nsg"
+#  description = "Name of the Network Security Group."
+#}
 
-variable "vnet_name" {
-  default     = "${var.name_prefix}_vnet"
-  description = "Name of the Virtual Network."
-}
+#variable "vnet_name" {
+#  type     = string
+#  #default     = "${var.name_prefix}_vnet"
+#  description = "Name of the Virtual Network."
+#}#>
